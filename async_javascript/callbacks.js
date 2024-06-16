925e4ec51b706d65e355ebde16bd7bb92d998764
kanchichauhan <k.chauhan27199@gmail.com>
2024-06-16 23:22:26 +0530

    callbacks added

--- async_javascript/callbacks.js
+++ async_javascript/callbacks.js
@@ -1,3 +1,24 @@
+// CALLBACK - A callback is a function passed as an argument to another function & a callback function can run after another function has finished
+
+// Sometimes you would like to have better control over when to execute a function.
+// Suppose you want to do a calculation, and then display the result.
+
+// Examples of callbacks are => 1. addEventListener, setTimeout, setInterval
+
+function displayResult (result) {
+    console.log(result)
+};
+function sumRes (num1, num2, func) {
+    func(num1+num2)
+}
+function minusRes (num1, num2, func) {
+    func(num1-num2)
+}
+sumRes(5, 2, displayResult);
+minusRes(5, 2, displayResult);
+
+/*----------------------- DISADVANTAGES ---------------------------------*/
+
 /**************** ❌ CALLBACK HELL/ PYRAMID OF DOOM *******************/
 const cart = ['shoes', 'pants', 'kurta'];
 // create order
