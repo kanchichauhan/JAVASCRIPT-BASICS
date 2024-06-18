a18c8a6d07d35bfed88c6df94f9e99df29ac33b0
kanchichauhan <k.chauhan27199@gmail.com>
2024-06-18 14:23:27 +0530

    async added

--- async_javascript/asyncawait.js
+++ async_javascript/asyncawait.js
@@ -21,16 +21,25 @@
         dataPromise.then(res => console.log(res));
 */
 
-const pr = new Promise((resolve, reject) => {
+const pr1 = new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve('promise resolved value');
-    }, 3000);
+    }, 5000);
+});
+const pr2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('promise resolved value');
+    }, 2000);
 });
 
 async function getData() {
     // await can only be used inside an async function
-    const data = await pr;
-    console.log(data);
-    console.log('work after async');
+    const data1 = await pr1;
+    console.log(data1); // it will wait until promise will be resolved
+    console.log('work after async1');
+
+    const data2 = await pr2;
+    console.log(data2); // it will wait until promise will be resolved
+    console.log('work after async2');
 }
 getData();
