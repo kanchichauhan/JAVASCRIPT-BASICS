90bc0fe27ca0120e9a5eefe5c29d8ae17dc2e09d
kanchichauhan <k.chauhan27199@gmail.com>
2024-07-21 21:46:40 +0530

    JS fundamentals

--- Prototype.js
+++ Prototype.js
@@ -11,9 +11,13 @@
     Properties or methods added to the prototype of a constructor function are accessible to all objects
     derived from it. For example,
 
+    ******* JavaScript Prototype Chaining *****
 
+    JavaScript always searches for properties in the objects of the constructor
+    function first. Then, it searches in the prototype.
 */
 
+// Inheritance
 function Car(model, year) {
     this.model = model;
     this.year = year;
@@ -38,3 +42,21 @@ console.log(`${c2.model} color: ${c2.color}`);
 // display added method using c1 and c2 objects
 c1.drive();
 c2.drive();
+
+
+// CHAINING
+
+function Car1() {
+    this.color = "Red";
+};
+
+// add property that already exists
+Car1.prototype.color = "Blue";
+
+// add a new property
+Car1.prototype.wheels = 4;
+
+const c3 = new Car1();
+
+console.log(`The car's color is ${c3.color}.`); 
+console.log(`The car has ${c3.wheels} wheels.`);
---
+++ objectMethods.js
@@ -0,0 +1,43 @@
+// ************** Object Methods **************** //
+
+// Copies properties from a source object to a target object
+Object.assign(target, source)
+
+// Creates an object from an existing object
+Object.create(object)
+
+// Returns an array of the key/value pairs of an object
+Object.entries(object)
+
+// Creates an object from a list of keys/values
+Object.fromEntries()
+
+// Returns an array of the keys of an object
+Object.keys(object)
+
+// Returns an array of the property values of an object
+Object.values(object)
+
+// Groups object elements according to a function
+Object.groupBy(object, callback)
+
+
+// ************** Object Properties **************** //
+
+// Adding or changing an object property
+Object.defineProperty(object, property, descriptor)
+
+// Adding or changing object properties
+Object.defineProperties(object, descriptors)
+
+// Accessing a Property
+Object.getOwnPropertyDescriptor(object, property)
+
+// Accessing Properties
+Object.getOwnPropertyDescriptors(object)
+
+// Returns all properties as an array
+Object.getOwnPropertyNames(object)
+
+// Accessing the prototype
+Object.getPrototypeOf(object)
